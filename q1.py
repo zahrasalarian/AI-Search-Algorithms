aad1f66596df919a85d0b87e03137b1502f28df2
amberbrown4 <zasalarian2000@gmail.com>
2020-12-09 17:17:08 +0330

    first commit

---
+++ q1.py
@@ -0,0 +1,45 @@
+# get input
+k, m, n = map(int, input().split())
+pack = []
+for _ in range(k):
+    pack.append(input().split())
+print(pack)
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier)  == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
